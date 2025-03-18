1ef055d90cb07794fc64ecaf7c71f9986bbe38e7
sedith <martin.jacquet@posteo.net>
2025-03-18 17:11:16 +0100

    stuff

---
+++ src/range_to_pc.py
@@ -0,0 +1,77 @@
+import os
+import numpy as np
+import torch
+import rospy
+from collision_predictor_mpc import COLPREDMPC_CONFIG_DIR
+from collision_predictor_mpc.utils.config import Config
+from collision_predictor_mpc.utils.visualization import Imgs2Points
+from collision_predictor_mpc.utils import preprocessing
+import ros_numpy
+from std_msgs.msg import Header, Float32
+from sensor_msgs.msg import Image, PointCloud2, PointField
+from sensor_msgs import point_cloud2
+
+
+class RosWrapper:
+    def __init__(self, pc2img):
+        rospy.init_node('range_to_pc')
+
+        self.cfg = cfg
+
+        self.preproc = torch.nn.Sequential(
+            preprocessing.ToDevice(self.cfg.nn.vae_device),
+            torch.jit.script(preprocessing.Reshape(self.cfg.sensor.shape_imgs)),
+            torch.jit.script(preprocessing.ClipDistance(self.cfg.sensor.dmax, self.cfg.sensor.mm_resolution)),
+            preprocessing.Morph(self.cfg.nn.vae_device),
+        )
+
+        self.img_to_points = Imgs2Points(
+            is_depth=self.cfg.sensor.is_depth,
+            is_spherical=self.cfg.sensor.is_spherical,
+            dmax=self.cfg.sensor.dmax,
+            hfov=self.cfg.sensor.hfov,
+            vfov=self.cfg.sensor.vfov,
+            downsamp=1,
+            remove_d0=True,
+            remove_dmax=True,
+            device=self.cfg.nn.vae_device,
+        )
+
+        topics = self.cfg.ros.topics
+        self.pub_pc = rospy.Publisher(f'{topics["obs"]}_pc', PointCloud2, tcp_nodelay=True, queue_size=1)
+        self.pub_min = rospy.Publisher(f'{topics["obs"]}_min', Float32, tcp_nodelay=True, queue_size=1)
+        self.sub_img = rospy.Subscriber(topics['obs'], Image, self.cb, tcp_nodelay=True, queue_size=1)
+
+        rospy.spin()
+
+    def pc_to_msg(self, ts, pc, norm=0.0, val=0.0):
+        data = np.zeros([pc.shape[0], 5], dtype=np.float32)
+        data[:, :3] = pc
+        data[:, 3] = norm
+        data[:, 4] = val
+        fields = [
+            PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
+            PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
+            PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
+            PointField(name='distance', offset=12, datatype=PointField.FLOAT32, count=1),
+            PointField(name='intensity', offset=16, datatype=PointField.FLOAT32, count=1),
+        ]
+
+        header = Header(stamp=ts, frame_id=self.cfg.ros.frames.body)
+        return point_cloud2.create_cloud(header, fields, data)
+
+    def cb(self, msg):
+        img = np.ndarray((msg.height, msg.width), self.cfg.sensor.dtype, msg.data, 0)
+        img = self.preproc(img)
+        pc = self.img_to_points(img)[0]
+        msg = self.pc_to_msg(msg.header.stamp, pc.cpu().numpy())
+        self.pub_pc.publish(msg)
+        self.pub_min.publish(Float32(min(torch.quantile(img[img > 0], 0.05).item() * self.cfg.sensor.dmax, 1)))
+
+
+
+if __name__ == '__main__':
+    cfg_file = f'params_{rospy.get_param("/cfg")}.yaml'
+
+    cfg = Config(os.path.join(COLPREDMPC_CONFIG_DIR, cfg_file))
+    RosWrapper(cfg)
--- src/viz_markers_node.py
+++ src/viz_markers_node.py
@@ -6,6 +6,9 @@ import rospy
 from std_msgs.msg import Header
 from nav_msgs.msg import Odometry, Path
 from visualization_msgs.msg import Marker
+from trajectory_msgs.msg import MultiDOFJointTrajectory
+from geometry_msgs.msg import Vector3
+
 
 class RosWrapper:
     def __init__(self, cfg):
@@ -17,13 +20,17 @@ class RosWrapper:
         self.pub_body = rospy.Publisher(topics.viz.markers['body'], Marker, tcp_nodelay=False, queue_size=1)
         self.pub_col = rospy.Publisher(topics.viz.markers['col'], Marker, tcp_nodelay=False, queue_size=1)
         self.pub_wp = rospy.Publisher(topics.viz.markers['wp'], Marker, tcp_nodelay=False, queue_size=1)
+        self.pub_dir = rospy.Publisher(topics.viz.markers['dir'], Marker, tcp_nodelay=False, queue_size=1)
         self.sub_state = rospy.Subscriber(topics['odom'], Odometry, self.cb_state, tcp_nodelay=True, queue_size=1)
         self.sub_wps = rospy.Subscriber(self.cfg.ros.topics['ref_wps'], Path, self.cb_wps, tcp_nodelay=True, queue_size=1)
+        self.sub_ref = rospy.Subscriber(self.cfg.ros.topics['ref_horizon'], MultiDOFJointTrajectory, self.cb_horizon, tcp_nodelay=True, queue_size=1)
 
         rospy.loginfo('node viz_markers started successfully')
         rospy.spin()
 
     def cb_state(self, msg):
+        self.state = msg.pose.pose
+
         msg_marker = Marker()
         msg_marker.header = Header(stamp=msg.header.stamp, frame_id=self.cfg.ros.frames.world)
         msg_marker.type = Marker.CYLINDER
@@ -64,6 +71,36 @@ class RosWrapper:
         msg_marker.color.g = 1.0
         msg_marker.color.b = 1.0
         self.pub_wp.publish(msg_marker)
+
+    def cb_horizon(self, msg):
+        ## direction to goal
+        msg_marker = Marker()
+        msg_marker.header = Header(stamp=msg.header.stamp, frame_id=self.cfg.ros.frames.world)
+
+        dir = np.array([
+            msg.points[-1].transforms[0].translation.x - self.state.position.x,
+            msg.points[-1].transforms[0].translation.y - self.state.position.y,
+            msg.points[-1].transforms[0].translation.z - self.state.position.z,
+        ])
+        norm = np.linalg.norm(dir)
+
+        if norm < 0.2:
+            msg_marker.action = Marker.DELETE
+            self.pub_dir.publish(msg_marker)
+        elif norm > 1:
+            dir = dir / norm
+        dir += np.array([self.state.position.x, self.state.position.y, self.state.position.z])
+        msg_marker.type = Marker.ARROW
+        msg_marker.points.append(self.state.position)
+        msg_marker.points.append(Vector3(*dir))
+        msg_marker.scale.x = 0.07  # shaft diameter
+        msg_marker.scale.y = 0.12  # head diameter
+        msg_marker.scale.z = 0.07  # head length
+        msg_marker.color.a = 1.0
+        msg_marker.color.r = 1.0
+        msg_marker.color.g = 1.0
+        msg_marker.color.b = 1.0
+        self.pub_dir.publish(msg_marker)
 
 
 if __name__ == '__main__':
